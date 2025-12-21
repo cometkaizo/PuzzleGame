8d9c5b27de4c141ab8e71f0bd35b6c3df4cfc46b
cometkaizo <andy.w.andmars@gmail.com>
2025-12-21 12:47:10 -0500

    add screen coords for mouse events

--- src/main/java/com/cometkaizo/game/event/MouseDownEvent.java
+++ src/main/java/com/cometkaizo/game/event/MouseDownEvent.java
@@ -3,6 +3,6 @@ package com.cometkaizo.game.event;
 import com.cometkaizo.event.Event;
 import com.cometkaizo.input.MouseButtonBinding;
 
-public record MouseDownEvent(MouseButtonBinding input, double x, double y) implements Event {
+public record MouseDownEvent(MouseButtonBinding input, double x, double y, int screenX, int screenY) implements Event {
 
 }
