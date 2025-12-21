3db4dbf2505c169fa99b10c51760e7fcc4f72631
cometkaizo <andy.w.andmars@gmail.com>
2025-12-21 15:51:05 -0500

    add volume modification

--- src/main/java/com/cometkaizo/screen/Sound.java
+++ src/main/java/com/cometkaizo/screen/Sound.java
@@ -1,8 +1,8 @@
 package com.cometkaizo.screen;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import com.cometkaizo.Main;
+
+import javax.sound.sampled.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
@@ -26,11 +26,30 @@ public class Sound {
         try {
             var clip = AudioSystem.getClip();
             clip.open(format, audio, 0, audio.length);
+
+            trySetVolume(clip, -15);
             clip.start();
         } catch (Exception e){
             e.printStackTrace();
         }
     }
+
+    private static void trySetVolume(Clip clip, float volume) {
+        if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+            var gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+            // ensure the value is within the control's limits
+            float minGain = gainControl.getMinimum();
+            float maxGain = gainControl.getMaximum();
+            float effectiveGain = Math.min(maxGain, Math.max(minGain, volume));
+
+            // set the volume in decibels
+            gainControl.setValue(effectiveGain);
+        } else {
+            Main.log("Volume control not supported for audio");
+        }
+    }
+
     public static void tick() {
         PLAYED_THIS_TICK.clear();
     }
