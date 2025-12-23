393b3607e6429c656974b76769fac5dd3c7d08e5
cometkaizo <andy.w.andmars@gmail.com>
2025-12-22 20:57:57 -0500

    add text component, letter text

---
+++ src/main/java/com/cometkaizo/screen/Text.java
@@ -0,0 +1,51 @@
+package com.cometkaizo.screen;
+
+import com.cometkaizo.util.StringUtils;
+
+import java.awt.*;
+import java.util.List;
+import java.util.function.IntUnaryOperator;
+
+/**
+ * Author: Andy Wang
+ * Date Modified: 2025-12-22
+ * Description: Renderable text component which handles all line-breaking logic. An instance of this class
+ * can be rendered at placed at any x and y, and with any width, font, or color.
+ */
+public class Text implements Renderable {
+    public List<String> lines;
+    public final String message;
+    private final Font font;
+    private final Color color;
+    private final IntUnaryOperator x, y;
+    private final int w;
+
+    public Text(String message, Font font, Color color, IntUnaryOperator x, IntUnaryOperator y, int w) {
+        this.message = message;
+        this.font = font;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+    }
+
+    @Override
+    public void render(Canvas canvas) {
+        // cannot do initialization of lines in the constructor because we don't have
+        // access to a Canvas instance (or we could pass in a Game instance to constructor)
+        if (lines == null) initLines(canvas);
+
+        for (int lineId = 0; lineId < lines.size(); lineId++) {
+            String line = lines.get(lineId);
+            int lineOffset = font.getSize() * (lineId + 1);
+            canvas.renderString(line, font, color,
+                    (float) (x.applyAsInt(canvas.getPixelWidth()) * canvas.renderScale()),
+                    (float) (y.applyAsInt(canvas.getPixelHeight()) * canvas.renderScale() + lineOffset),
+                    false, false);
+        }
+    }
+
+    private void initLines(Canvas canvas) {
+        lines = StringUtils.createLines(message, canvas.getGraphics().getFontMetrics(font), (int) (w * canvas.renderScale()));
+    }
+}
--- src/main/java/com/cometkaizo/screen/overlay/LetterOverlay.java
+++ src/main/java/com/cometkaizo/screen/overlay/LetterOverlay.java
@@ -3,10 +3,26 @@ package com.cometkaizo.screen.overlay;
 import com.cometkaizo.app.GameApp;
 import com.cometkaizo.screen.Assets;
 import com.cometkaizo.screen.Canvas;
+import com.cometkaizo.screen.Text;
 
 import java.awt.*;
 
 public class LetterOverlay extends Overlay {
+    private final Text content = new Text("""
+            Hello son,
+            
+            Here is the museum at last - take a deep breath and feel the history in the air.
+            
+            As we have agreed, you may take care of the museum until I return. You know that this museum is everything to me.
+            
+            I am writing from Egypt. The nine rulers have refused my generous offer for the final artifact in this collection, but they will not escape me. I shall be finding them in person to discuss the matter further.
+            
+            Sincerely,
+            Your father
+            """, Assets.font("BoldPixels", 20), Color.BLACK,
+            w -> w / 2 - 24,
+            h -> h / 2 - 28,
+            47);
 
     public LetterOverlay(GameApp app) {
         super(app);
@@ -16,10 +32,7 @@ public class LetterOverlay extends Overlay {
     public void render(Canvas canvas) {
         canvas.fillScreen(new Color(0, 0, 0, 200));
         canvas.renderImage(Assets.texture("gui/letter"), canvas.halfWidth(), canvas.halfHeight(), -0.5, -0.5);
-    }
 
-    @Override
-    public void tick() {
-
+        content.render(canvas);
     }
 }
