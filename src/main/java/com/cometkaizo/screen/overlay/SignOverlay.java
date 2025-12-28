44ed901b0d551bd042ad2cab3ac9f21082158cd4
cometkaizo <andy.w.andmars@gmail.com>
2025-12-28 00:55:18 -0500

    make sign

---
+++ src/main/java/com/cometkaizo/screen/overlay/SignOverlay.java
@@ -0,0 +1,35 @@
+package com.cometkaizo.screen.overlay;
+
+import com.cometkaizo.app.GameApp;
+import com.cometkaizo.screen.Assets;
+import com.cometkaizo.screen.Canvas;
+import com.cometkaizo.screen.Text;
+
+import java.awt.*;
+
+public class SignOverlay extends Overlay {
+    private final Text title, desc;
+    private final String variant;
+
+    public SignOverlay(GameApp app, String title, String desc, String variant) {
+        super(app);
+        this.title = new Text(title, Assets.font("BoldPixels", 40), Color.BLACK,
+                w -> w / 2 - 48,
+                h -> h / 2 - 60,
+                94, false);
+        this.desc = new Text(desc, Assets.font("BoldPixels", 20), Color.BLACK,
+                w -> w / 2 - 48,
+                h -> h / 2 - 30,
+                94, false);
+        this.variant = variant;
+    }
+
+    @Override
+    public void render(Canvas canvas) {
+        super.render(canvas);
+        canvas.renderImage(Assets.texture("gui/" + variant), canvas.halfWidth(), canvas.halfHeight(), -0.5, -0.5);
+
+        title.render(canvas);
+        desc.render(canvas);
+    }
+}
--- src/main/java/com/cometkaizo/world/entity/Sign.java
+++ src/main/java/com/cometkaizo/world/entity/Sign.java
@@ -1,21 +1,48 @@
 package com.cometkaizo.world.entity;
 
+import com.cometkaizo.screen.overlay.SignOverlay;
 import com.cometkaizo.world.Args;
 import com.cometkaizo.world.Room;
 import com.cometkaizo.world.Vector;
 
 public class Sign extends Interactable {
+
+    public static final Content[] CONTENT = {
+            new Content("Mayan Calendar"),
+            new Content("Gallery A"),
+            new Content("Gallery B"),
+            new Content("Gallery C", "China called itself the \"Central Kingdom\""),
+            new Content("Gallery D"),
+            new Content("Gallery E"),
+    };
+
+    private String title, desc;
+
     public Sign(Room.Layer layer, Vector.MutableDouble position, Args args) {
         super(layer, position, args);
     }
 
     @Override
+    public void reset() {
+        super.reset();
+        int messageVariant = originalArgs.nextInt(1) - 1;
+        title = CONTENT[messageVariant].title;
+        desc = CONTENT[messageVariant].desc;
+    }
+
+    @Override
     protected void interact() {
-        // todo: set overlay to letter overlay
+        app.setOverlay(new SignOverlay(app, title, desc, "sign"));
     }
 
     @Override
     protected String getTexturePath() {
         return "sign";
+    }
+
+    public record Content(String title, String desc) {
+        public Content(String title) {
+            this(title, "");
+        }
     }
 }
