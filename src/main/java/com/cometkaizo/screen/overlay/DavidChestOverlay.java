c1f84744b65b7013120d66d36bf19a85b3fffd1c
cometkaizo <andy.w.andmars@gmail.com>
2026-01-02 21:38:09 -0500

    check for organ key item

--- src/main/java/com/cometkaizo/screen/overlay/DavidChestOverlay.java
+++ src/main/java/com/cometkaizo/screen/overlay/DavidChestOverlay.java
@@ -2,6 +2,7 @@ package com.cometkaizo.screen.overlay;
 
 import com.cometkaizo.app.GameApp;
 import com.cometkaizo.game.event.MousePressedEvent;
+import com.cometkaizo.game.item.OrganKeyItem;
 import com.cometkaizo.screen.Assets;
 import com.cometkaizo.screen.Canvas;
 import com.cometkaizo.screen.Clickable;
@@ -16,11 +17,15 @@ public class DavidChestOverlay extends Overlay {
     public DavidChestOverlay(GameApp app, Runnable openHeartAction, Overlay prev) {
         super(app, prev);
         this.openHeartAction = openHeartAction;
-        keyhole = new ImageClickable(this.app, this::openHeart, w -> w/2 - 9, h -> h/2 - 22, _ -> 16, _ -> 16, () -> "gui/sculpture/david/keyhole", 0, 0);
+        keyhole = new ImageClickable(this.app, this::tryOpenHeart, w -> w/2 - 9, h -> h/2 - 22, _ -> 16, _ -> 16, () -> "gui/sculpture/david/keyhole", 0, 0);
     }
 
-    public void openHeart() {
-        // todo: do a check for key item in cursor here
+    public void tryOpenHeart() {
+        app.setOverlay(new InventoryOverlay(app, item -> {
+            if (item instanceof OrganKeyItem) openHeart();
+        }, this));
+    }
+    private void openHeart() {
         openHeartAction.run();
         heartOpen = true;
     }
