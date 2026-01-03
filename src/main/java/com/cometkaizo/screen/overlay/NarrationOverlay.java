4e33f0508d17555eb1efbc6ef6bb99f7685f6ea8
cometkaizo <andy.w.andmars@gmail.com>
2026-01-02 20:41:00 -0500

    add narration overlay

---
+++ src/main/java/com/cometkaizo/screen/overlay/NarrationOverlay.java
@@ -0,0 +1,29 @@
+package com.cometkaizo.screen.overlay;
+
+import com.cometkaizo.app.GameApp;
+import com.cometkaizo.screen.Assets;
+import com.cometkaizo.screen.Canvas;
+import com.cometkaizo.screen.Text;
+
+import java.awt.*;
+
+public class NarrationOverlay extends Overlay {
+    private final Text content;
+
+    public NarrationOverlay(GameApp app, String message) {
+        this(app, message, null);
+    }
+    public NarrationOverlay(GameApp app, String message, Overlay prev) {
+        super(app, prev);
+        content = new Text(message, Assets.font("BoldPixels", 50), Color.WHITE,
+                w -> w / 2,
+                h -> h / 2,
+                100, true, true);
+    }
+
+    @Override
+    public void render(Canvas canvas) {
+        super.render(canvas);
+        content.render(canvas);
+    }
+}
